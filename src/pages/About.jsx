0be70147c7099f898609d6850b4de3907fb96690
Maxwell <maxwell.nganga@student.moringaschool.com>
2023-08-02 14:43:16 +0300

    pulling rating

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,11 @@
+import AboutUsLayout from "../components/AboutUsLayout";
 import Navbar from "../components/Navbar";
 
 function About() {
   return (
     <div>
       <Navbar />
+      <AboutUsLayout />
     </div>
   );
 }
