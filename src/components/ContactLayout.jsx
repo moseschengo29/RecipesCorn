26512316ab609c269c188bd1acd5c5a4c41d4f4f
alvinkioi <franchisekioi@gmail.com>
2023-08-03 15:44:14 +0300

    Created a recipe page

--- src/components/ContactLayout.jsx
+++ src/components/ContactLayout.jsx
@@ -5,6 +5,7 @@ import Footer from "./Footer";
 function ContactLayout() {
   return (
     <>
+    <div className="contact-page">
       <div className="contact-heading">
         <h2 className="subheading ">Contact</h2>
         <h1 className="heading mg-btm">Got any questions ?</h1>
@@ -12,6 +13,7 @@ function ContactLayout() {
       <div className="contact">
         <ContactForm />
         <Map />
+      </div>
       </div>
       <Footer />
     </>
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,5 +1,51 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
+
+const API_KEY = "e234220dbda64365b25b5fe09b8b83d3";
+
 function App() {
-  return <div>App!</div>;
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    async function getRecipes() {
+      const response = await fetch(
+        `https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`
+      );
+      const data = await response.json();
+      setRecipes(data.recipes);
+    }
+    getRecipes();
+  }, []);
+
+  return (
+    <div>
+      <header>
+        <Link to='/'><h1>RecipesCorn</h1></Link>
+      </header>
+      <div className="container">
+        <ul className="recipe-list">
+          {recipes.map((recipe) => (
+            <li key={recipe.id} className="recipe-item">
+              <img src={recipe.image} alt="recipe" />
+              <h2>{recipe.title}</h2>
+              <p>
+                <strong>Ingredients:</strong>{" "}
+                {recipe.extendedIngredients.map((ingredient) => ingredient.original).join(", ")}
+              </p>
+              <a target='_blank' href={recipe.sourceUrl}>View Recipe</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
+
+
+
+
+
+
